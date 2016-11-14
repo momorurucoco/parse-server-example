edea636f15d728fb4baff57e476546eec104f2ee
momorurucoco <06251109tkm@gmail.com>
2016-11-14 22:22:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var api = new ParseServer({
 	        //pfx: __dirname + '/iPhoneP12/com.arata1972.japan.spa store.p12',
 		pfx: __dirname + '/iPhoneP12/com.arata1972.japan.spa dev.p12',
 	        bundleId: 'com.arata1972.japan.spa',
-		production: false
+		//production: false
 	        //production: true
 	      }
     }
