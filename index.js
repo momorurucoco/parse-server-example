8af54592a888436a878b69811cf4f4b9e8b51e72
momorurucoco <06251109tkm@gmail.com>
2020-08-18 17:17:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-	  databaseURI: databaseUri || 'mongodb://heroku_dmrs6xn0:pc48lbn6u2qtr5b9dloechs942@ds139817.mlab.com:39817/heroku_dmrs6xn0',
+	databaseURI: databaseUri,  
+	//databaseURI: databaseUri || 'mongodb://heroku_dmrs6xn0:pc48lbn6u2qtr5b9dloechs942@ds139817.mlab.com:39817/heroku_dmrs6xn0',
 	  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
 	  appId: process.env.APP_ID || 'md6b718xbdyl',
 	  masterKey: process.env.MASTER_KEY || 'eruqp70env57', //Add your master key here. Keep it secret!
